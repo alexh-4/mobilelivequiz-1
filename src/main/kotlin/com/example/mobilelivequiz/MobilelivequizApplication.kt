e0be4efe7f03a006ae75cfba49c85fc78312e93b
Benedict <benedicth2468@gmail.com>
2024-11-14 14:28:52 +0000

    Initial commit

---
+++ src/main/kotlin/com/example/mobilelivequiz/MobilelivequizApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mobilelivequiz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MobilelivequizApplication
+
+fun main(args: Array<String>) {
+	runApplication<MobilelivequizApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/mobilelivequiz/MobilelivequizApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mobilelivequiz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MobilelivequizApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
